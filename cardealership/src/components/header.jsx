240a5059319f0476fc019384e27ba8d2c4071f1e
Daniel Chacha <115540535+CardenDante@users.noreply.github.com>
2025-03-30 08:23:32 +0000

    intial create

---
+++ cardealership/src/components/header.jsx
@@ -0,0 +1,7 @@
+export default function Nav() {
+    return (
+        <header className="w-full bg-blue-500 text-white p-4 text-center">
+            <h2 className="text-lg font-semibold">My Website Header</h2>
+        </header>
+    );
+}
